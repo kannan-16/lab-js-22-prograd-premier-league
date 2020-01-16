b35fcd88937a52d33a7b8e0d3ecc78a40e37dc17
Hamza <hamza.kaizar@gmail.com>
2020-01-16 15:06:46 +0530

    uploads lab21 face premier league

---
+++ src/app.js
@@ -0,0 +1,40 @@
+//Trial 1 - create a Manager array and return it
+let managerName = "Alex Ferguson";
+let managerAge = 78;
+let currentTeam = "Manchester FC";
+let trophiesWon = 27;
+
+
+//Trial 2 - create a formation object and return it
+let formation = [2, 3, 2];
+
+
+//Trial 3 - Filter players that debuted in ___ year
+
+
+//Trial 4 - Filter players that play at the position _______
+
+
+//Trial 5 - Filter players that have won ______ award
+
+
+//Trial 6 - Filter players that won ______ award ____ times
+
+
+//Trial 7 - Filter players that won ______ award and belong to ______ country
+
+
+//Trial 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+
+
+//Challenge 1 - Sort players in descending order of their age
+
+
+//Challenge 2 - Sort players beloging to _____ team in descending order of awards won
+
+
+//Judgement 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+
+
+/*Judgement 2 - Sort players that are older than _____ years in alphabetical order
+                Sort the awards won by them in reverse chronological order*/

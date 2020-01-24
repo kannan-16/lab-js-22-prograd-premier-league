551ada2327ccbf3067fa4229a457fda9f68d914e
Venkat <raghul.v@gmail.com>
2020-01-24 19:26:50 +0530

    final

--- src/app.js
+++ src/app.js
@@ -5,22 +5,43 @@ let currentTeam = "Manchester FC";
 let trophiesWon = 27;
 
 function createManager(managerName, managerAge, currentTeam, trophiesWon) {
-  // return [managerName, managerAge, currentTeam, trophiesWon];
+  return [managerName, managerAge, currentTeam, trophiesWon];
 }
 
-var manager = createManager(managerName, managerAge, currentTeam, trophiesWon);
+// Don't edit the following code
+try {
+  var manager = createManager(
+    managerName,
+    managerAge,
+    currentTeam,
+    trophiesWon
+  );
+} catch (e) {
+  // do nothing - expected error
+}
 
 //Trial 2 - create a formation object and return it
 var formation = [4, 2, 4];
 
-function updateFormation(formation) {
-  // return {
-  //   defenders: formation[0],
-  //   midfielders: formation[1],
-  //   forwards: formation[2]
-  // };
+function createFormation(formation) {
+  if (formation.length == 0) {
+    return null;
+  } else {
+    return {
+      defender: formation[0],
+      midfield: formation[1],
+      forward: formation[2]
+    };
+  }
 }
-var formationObject = updateFormation(formation);
+
+// Dont edit the following code
+
+try {
+  var formationObject = createFormation(formation);
+} catch (e) {
+  //do nothing
+}
 
 //Trial 3 - Filter players that debuted in ___ year
 
@@ -36,116 +57,123 @@ function filterByDebut(year) {
 }
 
 //Trial 4 - Filter players that play at the position _______
+
 function filterByPosition(position) {
-  // let filteredArray = [];
-  // let count = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   if (players[i].position === position) {
-  //     filteredArray[count++] = players[i];
-  //   }
-  // }
-  // return filteredArray;
+  let filteredArray = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].position === position) {
+      filteredArray[count++] = players[i];
+    }
+  }
+  return filteredArray;
 }
 
 //Trial 5 - Filter players that have won ______ award
+
 function filterByAward(awardName) {
-  // let filteredArray = [];
-  // let count = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   for (let j = 0; j < players[i].awards.length; j++) {
-  //     if (players[i].awards[j].name === awardName) {
-  //       filteredArray[count++] = players[i];
-  //       break;
-  //     }
-  //   }
-  // }
-  // return filteredArray;
+  let filteredArray = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++) {
+    for (let j = 0; j < players[i].awards.length; j++) {
+      if (players[i].awards[j].name === awardName) {
+        filteredArray[count++] = players[i];
+        break;
+      }
+    }
+  }
+  return filteredArray;
 }
 
 //Trial 6 - Filter players that won ______ award ____ times
+
 function filterByAwardxTimes(awardName, timesWon) {
-  // let filteredArray = [];
-  // let count = 0,
-  //   tempTimes = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   tempTimes = 0;
-  //   for (let j = 0; j < players[i].awards.length; j++) {
-  //     if (players[i].awards[j].name == awardName) {
-  //       if (++tempTimes >= timesWon) {
-  //         filteredArray[count++] = players[i];
-  //       }
-  //     }
-  //   }
-  // }
-  // return filteredArray;
+  let filteredArray = [];
+  let count = 0,
+    tempTimes = 0;
+  for (let i = 0; i < players.length; i++) {
+    tempTimes = 0;
+    for (let j = 0; j < players[i].awards.length; j++) {
+      if (players[i].awards[j].name == awardName) {
+        if (++tempTimes >= timesWon) {
+          filteredArray[count++] = players[i];
+        }
+      }
+    }
+  }
+  return [...new Set(filteredArray)];
 }
 
 //Trial 7 - Filter players that won ______ award and belong to ______ country
+
 function filterByAwardxCountry(awardName, country) {
-  // let filteredArray = [];
-  // let count = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   if (players[i].country == country) {
-  //     for (let j = 0; j < players[i].awards.length; j++) {
-  //       if (players[i].awards[j].name == awardName) {
-  //         filteredArray[count++] = players[i];
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
-  // return filteredArray;
+  let filteredArray = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].country == country) {
+      for (let j = 0; j < players[i].awards.length; j++) {
+        if (players[i].awards[j].name == awardName) {
+          filteredArray[count++] = players[i];
+          break;
+        }
+      }
+    }
+  }
+  return filteredArray;
 }
 
 //Trial 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+
 function filterByNoOfAwardsxTeamxAge(noOfAwards, team, age) {
-  // let filteredArray = [];
-  // let count = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   if (
-  //     players[i].awards.length >= noOfAwards &&
-  //     players[i].team == team &&
-  //     players[i].age <= age
-  //   ) {
-  //     filteredArray[count++] = players[i];
-  //   }
-  // }
-  // return filteredArray;
+  let filteredArray = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (
+      players[i].awards.length >= noOfAwards &&
+      players[i].team == team &&
+      players[i].age <= age
+    ) {
+      filteredArray[count++] = players[i];
+    }
+  }
+
+  return filteredArray;
 }
 
 //Challenge 1 - Sort players in descending order of their age
+//Challenge 1 - Sort players in descending order of their age
 function sortByAge() {
-  // for (let i = 0; i < players.length; i++) {
-  //   for (let j = i + 1; j < players.length; j++) {
-  //     if (players[i].age < players[j].age) {
-  //       let temp = players[i];
-  //       players[i] = players[j];
-  //       players[j] = temp;
-  //     }
-  //   }
-  // }
-  // return players;
+  for (let i = 0; i < players.length; i++) {
+    for (let j = i + 1; j < players.length; j++) {
+      if (players[i].age < players[j].age) {
+        let temp = players[i];
+        players[i] = players[j];
+        players[j] = temp;
+      }
+    }
+  }
+  return players;
 }
 
 //Challenge 2 - Sort players beloging to _____ team in descending order of awards won
 function filterByTeamxSortByNoOfAwards(team) {
-  // let count = 0;
-  // let filteredPlayers = [];
-  // for (let i = 0; i < players.length; i++) {
-  //   if (players[i].team == team) {
-  //     filteredPlayers[count++] = players[i];
-  //   }
-  // }
-  // for (let i = 0; i < filteredPlayers.length; i++) {
-  //   for (let j = i + 1; j < filteredPlayers.length; j++) {
-  //     if (filteredPlayers[i].awards.length < filteredPlayers[j].awards.length) {
-  //       let temp = filteredPlayers[i];
-  //       filteredPlayers[i] = filteredPlayers[j];
-  //       filteredPlayers[j] = temp;
-  //     }
-  //   }
-  // }
-  // return filteredPlayers;
+  let count = 0;
+  let filteredPlayers = [];
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].team == team) {
+      filteredPlayers[count++] = players[i];
+    }
+  }
+  for (let i = 0; i < filteredPlayers.length; i++) {
+    for (let j = i + 1; j < filteredPlayers.length; j++) {
+      if (filteredPlayers[i].awards.length < filteredPlayers[j].awards.length) {
+        let temp = filteredPlayers[i];
+        filteredPlayers[i] = filteredPlayers[j];
+        filteredPlayers[j] = temp;
+      }
+    }
+  }
+  return filteredPlayers;
 }
 
 //Judgement 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
@@ -163,22 +191,22 @@ function sortByPlayerName(players) {
 }
 
 function sortByNamexAwardxTimes(awardName, noOfTimes, country) {
-  // let filteredArray = [];
-  // let count = 0;
-  // let aCount = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   if (players[i].country === country) {
-  //     aCount = 0;
-  //     for (let j = 0; j < players[i].awards.length; j++) {
-  //       if (players[i].awards[j].name === awardName) {
-  //         ++aCount;
-  //       }
-  //     }
+  let filteredArray = [];
+  let count = 0;
+  let aCount = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].country === country) {
+      aCount = 0;
+      for (let j = 0; j < players[i].awards.length; j++) {
+        if (players[i].awards[j].name === awardName) {
+          ++aCount;
+        }
+      }
 
-  //     if (aCount == noOfTimes) filteredArray[count++] = players[i];
-  //   }
-  // }
-  // return sortByPlayerName(filteredArray);
+      if (aCount == noOfTimes) filteredArray[count++] = players[i];
+    }
+  }
+  return sortByPlayerName(filteredArray);
 }
 
 /*Judgement 2 - Sort players that are older than _____ years in alphabetical order
@@ -197,16 +225,16 @@ function sortAwardsByYear(awards) {
 }
 
 function sortByNamexOlderThan(age) {
-  // let filteredArray = [];
-  // let count = 0;
-  // for (let i = 0; i < players.length; i++) {
-  //   if (players[i].age >= age) {
-  //     filteredArray[count] = players[i];
-  //     filteredArray[count].awards = sortAwardsByYear(
-  //       filteredArray[count].awards
-  //     );
-  //     count++;
-  //   }
-  // }
-  // return sortByPlayerName(filteredArray);
+  let filteredArray = [];
+  let count = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].age >= age) {
+      filteredArray[count] = players[i];
+      filteredArray[count].awards = sortAwardsByYear(
+        filteredArray[count].awards
+      );
+      count++;
+    }
+  }
+  return sortByPlayerName(filteredArray);
 }
--- src/link.js
+++ src/link.js
@@ -1,11 +1,11 @@
 const managerDetails = document.querySelector(".grid-container").children;
 //checking trial 1
 try {
-  [...managerDetails].forEach(function (element, index) {
+  [...managerDetails].forEach(function(element, index) {
     element.querySelector(".insidetext").textContent = manager[index];
   });
 } catch (e) {
-  console.log(e);
+  //Ignore error as we don't have the function yet
 }
 
 //checking trial 2
@@ -14,23 +14,24 @@ const midfielders = document.querySelector(".midfield");
 const forwards = document.querySelector(".forward");
 
 try {
-  for (let i = 0; i < formationObject.defenders; i++) {
+  for (let i = 0; i < formationObject.defender; i++) {
     defenders.innerHTML += `<img src="./assets/player.svg" alt="" class="defenders"></img>`;
   }
-  for (let i = 0; i < formationObject.midfielders; i++) {
+  for (let i = 0; i < formationObject.midfield; i++) {
     midfielders.innerHTML += `<img src="./assets/player.svg" alt="" class="defenders"></img>`;
   }
-  for (let i = 0; i < formationObject.forwards; i++) {
+  for (let i = 0; i < formationObject.forward; i++) {
     forwards.innerHTML += `<img src="./assets/player.svg" alt="" class="defenders"></img>`;
   }
 } catch (e) {
-  console.log(e);
+  //Ignore error as we don't have the function yet
 }
 
 //populate all players
-const playerContainer = document.querySelector(".squad");
+const playerContainer = document.getElementById("player-container");
 
 function createCards(players) {
+  playerContainer.innerHTML = "";
   players.forEach(player => {
     let awardsTpl = "";
     player["awards"].forEach(element => {
@@ -77,137 +78,261 @@ function createCards(players) {
   });
 }
 
+function resetselection() {
+  document.getElementById("awardflag").textContent = "";
+  document.getElementById("positionflag").textContent = "";
+  document.getElementById("debutflag").textContent = "";
+  document.getElementById("teamflag").textContent = "";
+  document.querySelectorAll(".award-selected").forEach(e => {
+    e.style.display = "none";
+  });
+  document.querySelectorAll(".team-selected").forEach(e => {
+    e.style.display = "none";
+  });
+  document
+    .getElementById("filter-type")
+    .querySelector(".subtitle.filtertype-active")
+    .classList.remove("filtertype-active");
+}
+
+//setup filter and sort menu
+document.querySelector(
+  ".filter-container"
+).firstElementChild.onclick = filtermenusetup;
+
+function filtermenusetup(evt) {
+  createCards([]);
+
+  try {
+    //deactivate the class filtertype-active for any selected buttons
+    resetselection();
+  } catch (e) {
+    //do nothing
+  }
+  this.classList.add("filtertype-active");
+  //display filtermenu
+  document.getElementById("filter-type").style.display = "inherit";
+}
+
 //populate filter elements
 //all player positions
 const positions = [...new Set(players.map(e => e.position))].sort();
-positions.forEach(e => document.querySelector("#dropdown-position").innerHTML += "<p class=\"position\">" + e + "</p>");
+positions.forEach(
+  e =>
+    (document.querySelector("#dropdown-position").innerHTML +=
+      '<p class="position">' + e + "</p>")
+);
+
+//position clickhandler and listener
+
+let positionEls = document.querySelectorAll(".position");
+positionEls.forEach(element => {
+  element.onclick = positionHandler;
+});
+
+function positionHandler(evt) {
+  try {
+    createCards(filterByPosition(evt.target.innerHTML));
+    try {
+      //deactivate the class filtertype-active for any selected buttons
+      resetselection();
+    } catch (e) {
+      //do nothing
+    }
+    //activate the class filtertype-active for position button
+    evt.target.parentElement.previousSibling.classList.add("filtertype-active");
+    document.getElementById("positionflag").textContent =
+      evt.target.textContent;
+  } catch (e) {
+    //do nothing as the function is not defined
+  }
+}
 
 //all debut years (sorted)
 const debutYears = [...new Set(players.map(e => e.debut))].sort();
-debutYears.forEach(e => document.querySelector("#dropdown-debut").innerHTML += "<p class=\"year\">" + e + "</p>");
+debutYears.forEach(e => {
+  document.querySelector("#dropdown-debut").innerHTML +=
+    '<p class="year">' + e + "</p>";
+});
+
+//debut year click listener and handler
+
+let debutYearEls = document.getElementsByClassName("year");
+
+[...debutYearEls].forEach(element => {
+  element.onclick = debutHandler;
+});
+
+function debutHandler(evt) {
+  try {
+    createCards(filterByDebut(evt.target.textContent));
+    try {
+      //deactivate the class filtertype-active for any selected buttons
+      resetselection();
+    } catch (e) {
+      //do nothing
+    }
+    //activate the class filtertype-active for position button
+    evt.target.parentElement.previousSibling.classList.add("filtertype-active");
+    document.getElementById("debutflag").textContent = evt.target.textContent;
+  } catch (e) {
+    //do nothing - expected error
+  }
+}
 
 //all awards won (sorted)
 const awards = [...new Set(players.map(e => e.awards))];
-//max no of awards won by any player
+// //max no of awards won by any player
 var maxAwardCount = 0;
 const awardNames = [];
 players.forEach(e => {
-  maxAwardCount = (maxAwardCount < e.awards.length) ? e.awards.length : maxAwardCount;
-  e.awards.forEach(f => (awardNames.indexOf(f.name) == -1) ? awardNames.push(f.name) : "")
+  maxAwardCount =
+    maxAwardCount < e.awards.length ? e.awards.length : maxAwardCount;
+  e.awards.forEach(f =>
+    awardNames.indexOf(f.name) == -1 ? awardNames.push(f.name) : ""
+  );
 });
 awardNames.sort();
-awardNames.forEach(e => document.querySelector("#dropdown-award").innerHTML += "<p class=\"award\">" + e + "</p>");
+awardNames.forEach(
+  e =>
+    (document.querySelector("#dropdown-award").innerHTML +=
+      '<p class="award">' + e + "</p>")
+);
+
+//award click listener
+
+let awardEls = document.getElementsByClassName("award");
+
+[...awardEls].forEach(element => {
+  element.onclick = awardHandler;
+});
+
+//advanced filters based on award
+
+function createAdvancedFilters(award) {
+  document.querySelectorAll(".award-selected").forEach(e => {
+    e.style.display = "block";
+  });
+}
+
+// award click handler
+function awardHandler(evt) {
+  try {
+    createCards(filterByAward(evt.target.textContent));
+    try {
+      //deactivate the class filtertype-active for any selected buttons
+      resetselection();
+    } catch (e) {
+      //do nothing
+    }
+    //activate the class filtertype-active for position button
+    evt.target.parentElement.previousElementSibling.classList.add(
+      "filtertype-active"
+    );
+    createAdvancedFilters(evt.target.textContent);
+    document.getElementById("awardflag").textContent = evt.target.textContent;
+  } catch (e) {
+    //do nothing - expected error
+  }
+}
+
+//number of awards listener and handler
+
+document.getElementById("awardCount").oninput = awardCountHandler;
+
+function awardCountHandler(evt) {
+  try {
+    let award = document.getElementById("awardflag").textContent;
+    let timesInput = evt.target;
+    let times = timesInput.value;
+    createCards(filterByAwardxTimes(award, times));
+  } catch (e) {
+    //do nothing
+  }
+}
 
 //all player countries
 const countries = [...new Set(players.map(e => e.country))].sort();
-countries.forEach(e => document.querySelector("#dropdown-country").innerHTML += "<p>" + e + "</p>");
+countries.forEach(
+  e =>
+    (document.querySelector("#dropdown-country").innerHTML +=
+      "<p>" + e + "</p>")
+);
+
+// country listener and handler
+
+document.getElementById("dropdown-country").onclick = countryHandler;
+function countryHandler(evt) {
+  try {
+    let award = document.getElementById("awardflag").textContent;
+    let country = evt.target.textContent;
+    createCards(filterByAwardxCountry(award, country));
+  } catch (e) {
+    //do nothing
+  }
+}
 
 //all player teams
 const teams = [...new Set(players.map(e => e.team))].sort();
-teams.forEach(e => document.querySelector("#dropdown-team").innerHTML += "<p>" + e + "</p>");
+teams.forEach(
+  e =>
+    (document.querySelector("#dropdown-team").innerHTML += "<p>" + e + "</p>")
+);
+
+//teams listener and handler
+document.getElementById("dropdown-team").onclick = teamHandler;
+
+function teamHandler(evt) {
+  createCards([]);
+  try {
+    resetselection();
+    // createCards(filterByNoOfAwardsxTeamxAge(noOfAwards, team, age));
+  } catch (e) {
+    //do nothing
+  }
+  document.querySelectorAll(".team-selected").forEach(e => {
+    e.style.display = "block";
+  });
+  evt.target.parentElement.previousElementSibling.classList.add(
+    "filtertype-active"
+  );
+  document.getElementById("teamflag").textContent = evt.target.textContent;
+
+  document.getElementById("goButton").onclick = goButtonHandler;
+
+  function goButtonHandler() {
+    try {
+      awardsTotal = document.getElementById("awardCountTotal");
+      noOfAwards = awardsTotal.value;
+      ageBox = document.getElementById("age");
+      age = ageBox.value;
+      team = document.getElementById("teamflag").textContent;
+      createCards(filterByNoOfAwardsxTeamxAge(noOfAwards, team, age));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+let sortButtons = document.getElementById("sort-type").children;
+sortButtons.onclick = sortButtonHandler;
+
+function sortButtonHandler(evt) {
+  switch (evt.target.textContent) {
+    case "Oldest to Newest":
+      createCards(sortByAge());
+      break;
+    case "More Awards":
+
+    case "Alphabetical Sort":
+    case "Alphabetical x Award Sort":
+  }
+}
 
 //max and min age among players (for age slider)
-var minAge = 100;
-var maxAge = 0;
-players.forEach(e => {
-  minAge = (minAge > e.age) ? e.age : minAge;
-  maxAge = (maxAge < e.age) ? e.age : maxAge;
-});
-
-//checking trial 3
-try {
-  createCards(filterByDebut(year));
-} catch (e) {
-  console.log(e);
-}
-
-//checking trial 4
-try {
-  createCards(filterByPosition(position));
-} catch (e) {
-  console.log(e);
-}
-
-//checking trial 5
-try {
-  createCards(filterByAward(awardName));
-} catch (e) {
-  console.log(e);
-}
-
-//checking trial 6
-try {
-  createCards(filterByAwardxTimes(awardName, timesWon));
-} catch (e) {
-  console.log(e);
-}
-
-//checking trial 7
-try {
-  createCards(filterByAwardxCountry(awardName, country));
-} catch (e) {
-  console.log(e);
-}
-
-//checking trial 8
-try {
-  createCards(filterByNoOfAwardsxTeamxAge(noOfAwards, team, age));
-} catch (e) {
-  console.log(e);
-}
-
-//checking challenge 1
-try {
-  createCards(sortByAge());
-} catch (e) {
-  console.log(e);
-}
-
-//checking challenge 2
-try {
-  createCards(filterByTeamxSortByNoOfAwards(team));
-} catch (e) {
-  console.log(e);
-}
-
-//checking judgement 1
-try {
-  createCards(sortByNamexAwardxTimes(awardName, noOfTimes, country));
-} catch (e) {
-  console.log(e);
-}
-
-//checking judgement 2
-try {
-  createCards(sortByNamexOlderThan(age));
-} catch (e) {
-  console.log(e);
-}
-
-//filter events
-//debut year click handler
-
-var debutClick = function (e) {
-  console.log(e.target.innerHTML + " clicked");
-  createCards(filterByDebut(e.target.innerHTML));
-}
-let debutYearEls = document.querySelectorAll(".year");
-debutYearEls.forEach(e => e.onclick = function (evt) {
-  debutClick(evt);
-});
-
-//position click handler
-var positionClick = function (e) {
-  console.log(e.target.innerHTML + " clicked");
-  createCards(filterByPosition(e.target.innerHTML));
-}
-let positionEls = document.querySelectorAll(".position");
-positionEls.forEach(e => e.onclick = positionClick);
-
-//award click handler
-var awardClick = function (e) {
-  console.log(e.target.innerHTML + " clicked");
-  createCards(filterByAward(e.target.innerHTML));
-}
-let awardEls = document.querySelectorAll(".award");
-awardEls.forEach(e => e.onclick = awardClick);
+// var minAge = 100;
+// var maxAge = 0;
+// players.forEach(e => {
+//   minAge = minAge > e.age ? e.age : minAge;
+//   maxAge = maxAge < e.age ? e.age : maxAge;
+// });
